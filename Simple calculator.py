babba4758418cea405e035a3df98408aec68c1e3
Honeyfone <jclam4goal@gmail.com>
2022-05-21 00:15:26 +0100

    Simple Calculator

---
+++ Simple calculator.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 20 22:19:22 2022
+
+@author: OLADELE KAYODE
+"""
+print("Hi :) This is your simple calculator")
+
+a = float(input("please enter the first number: "))
+
+b = float(input("please enter the second number: "))
+
+print(f"{a}+{b} is {round((a)+(b))},{b}-{a} is {round(b-(a))}, {b/a} is {round(b/(a))}, {b}*{a} is {round(b*(a))}")
